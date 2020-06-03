5f90d23a47d12faa4de8d1516620ffb6e7fa236e
Gayeon Lee <rkdus1006@gmail.com>
2020-06-04 02:36:22 +0900

    carousel 양쪽 마지막 슬라이드 수정

--- index.js
+++ index.js
@@ -1,68 +1,91 @@
-const slide_item = document.querySelector('.slide:first-child');
+const slide_element = document.querySelectorAll('.slide_item');
+const first = document.querySelector('.slide:first-child');
+const last = document.querySelector('.slide:last-child');
 const slide_list = document.querySelector('.slide_list');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-const slide_element = document.querySelectorAll('.slide_item');
+const first_slide = first.nextElementSibling;
+const last_slide = last.previousElementSibling;
+
+const check_btn = document.querySelectorAll('.check_btn');
 
 const ACTIVE_CLASS = 'active';
 const SIZE = slide_element[0].clientWidth;
-let count = 0;
+const S_LENGTH = slide_element.length - 2;
+
+let count = 1;
 
 //자동 슬라이드
 function slide_event(){
     const current_child = document.querySelector(`.${ACTIVE_CLASS}`);
-    if(count !== 5){
-        current_child.classList.remove(`${ACTIVE_CLASS}`);
-        const next_slide = current_child.nextElementSibling;
-        next_slide.classList.add(`${ACTIVE_CLASS}`);
-    
-        count++;
-        slide_list.style.transition = "transform 0.6s";
-        slide_list.style.transform = 'translate3d(' + -(SIZE*count) + 'px, 0, 0)';
-    }
-    //5슬라이드->1슬라이드로 넘어가기
-    else if(count === 5){
-        count = 0;
+    const next_slide = current_child.nextElementSibling;
 
-        current_child.classList.remove(`${ACTIVE_CLASS}`);
-        slide_item.classList.add(`${ACTIVE_CLASS}`);
-        slide_list.style.transition = "transform 0s";
-        slide_list.style.transform = 'translate3d(' + -(SIZE*count) + 'px, 0, 0)';
+    count++;
+
+    slide_list.style.transition = "transform 0.6s";
+    slide_list.style.transform = 'translateX(' + -(SIZE*count) + 'px)';
+
+    current_child.classList.remove(`${ACTIVE_CLASS}`);
+
+    if(count === S_LENGTH+2){
+        first_slide.classList.add(`${ACTIVE_CLASS}`);
+        count = 1;
+        slide_list.style.removeProperty('transition');
+        slide_list.style.transform = 'translateX(' + -(SIZE*count) + 'px)';
     } 
+    else{
+        next_slide.classList.add(`${ACTIVE_CLASS}`);
+    }
+    
 }
+
 
 //prevBtn 클릭
 function prevBtn_event(e){
-    if(count !== 0){
-        const current_child = document.querySelector(`.${ACTIVE_CLASS}`);
-        current_child.classList.remove(`${ACTIVE_CLASS}`);
+    count --;
+    const current_child = document.querySelector(`.${ACTIVE_CLASS}`);
+    current_child.classList.remove(`${ACTIVE_CLASS}`);
 
+    slide_list.style.transition = "transform 0.6s";
+    slide_list.style.transform = 'translateX(' + -(SIZE*count) + 'px)';
+    
+    if(count === -1){
+        last_slide.classList.add(`${ACTIVE_CLASS}`);
+        count = S_LENGTH;
+        slide_list.style.removeProperty('transition');
+        slide_list.style.transform = 'translateX(' + -(SIZE*count) + 'px)';
+    }
+    else{
         const prev_slide = current_child.previousElementSibling;
         prev_slide.classList.add(`${ACTIVE_CLASS}`);
-
-        count --;
-        slide_list.style.transition = "transform 0.6s";
-        slide_list.style.transform = 'translate3d(' + -(SIZE*count) + 'px, 0, 0)';
     }
 }
+
 //nextBtn 클릭
 function nextBtn_event(e){
-    if(count < 5){
-        const current_child = document.querySelector(`.${ACTIVE_CLASS}`);
-        current_child.classList.remove(`${ACTIVE_CLASS}`);
+    count ++;
+    const current_child = document.querySelector(`.${ACTIVE_CLASS}`);
+    current_child.classList.remove(`${ACTIVE_CLASS}`);
 
+    slide_list.style.transition = "transform 0.6s";
+    slide_list.style.transform = 'translateX(' + -(SIZE*count) + 'px)';
+
+    if(count === 6){
+        first_slide.classList.add(`${ACTIVE_CLASS}`);
+        count = 0;
+        slide_list.style.removeProperty('transition');
+        slide_list.style.transform = 'translateX(' + -(SIZE*count) + 'px)';
+    }
+    else{
         const next_slide = current_child.nextElementSibling;
         next_slide.classList.add(`${ACTIVE_CLASS}`);
-
-        count ++;
-        slide_list.style.transition = "transform 0.6s";
-        slide_list.style.transform = 'translate3d(' + -(SIZE*count) + 'px, 0, 0)';
     }
+    
 }
 
 //실행
 function slide(){
-    slide_item.classList.add('active');
+    first_slide.classList.add(`${ACTIVE_CLASS}`);
     setInterval(slide_event, 2000);
     prevBtn.addEventListener('click', prevBtn_event);
     nextBtn.addEventListener('click', nextBtn_event);
